4c5b8ae3c2ab059deb18033a3c00ee84f3f48d09
hang1038 <31604301+hang1038@users.noreply.github.com>
2018-02-17 23:37:20 +0800

    Minor fixes

--- server.js
+++ server.js
@@ -14,138 +14,141 @@ var dbName = 'report';
 var apikey = '0d8144c5-4df7-4953-b813-f1104fe86dd1';
 
 //The below part is setting up the requirement
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+
 app.use(cookieSession({
 	name: 'session',
 	keys: ['key1', 'key2'],
 	maxAge: 30 * 60 * 1000
 }));
 
-app.use(function(req,res,next){
+app.use(function(req, res, next) {
 	res.locals.session = req.session;
 	next();
 });
 
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: true })); 
-app.use(bodyParser.json());
-app.set('view engine', 'ejs');
+function auth(req, res, next) {
+	if (req.session.authenticated)
+		next();
+	else
+		res.redirect('/login');
+}
 
-/**
- * Route handlers
- */
+// Routing
+app.post('/test', function(req, res, next) {
+	var userInfo = {};
+	var score = {};
+	var datetime = new Date();
+	var clientapi = req.body.api;
+	var currentlevel = req.body.currentlevel;
+	userInfo['username'] = req.body.username;
+	score['date'] = datetime;
+	score['level' + currentlevel] = req.body.levelscore;
+	userInfo['Score'] = score;
 
- app.post('/test' ,  function(req, res, next) {
- 	var userInfo = {};
- 	var score = {};
- 	var datetime = new Date();
- 	var clientapi = req.body.api;
- 	var currentlevel = req.body.currentlevel;
- 	userInfo['username'] = req.body.username;
- 	score['date'] = datetime;
- 	score['level' + currentlevel] = req.body.levelscore;
- 	userInfo['Score'] = score;
+	if (clientapi == apikey){
+		MongoClient.connect(mongourl, function(err, client) {
+			assert.equal(null, err);
+			const db = client.db(dbName);
 
- 	if (clientapi == apikey){
- 		MongoClient.connect(mongourl, function(err, client) {
- 			assert.equal(null, err);
- 			const db = client.db(dbName);
+			console.log('Connected to MongoDB\n');
+			insertRecord(db,userInfo,function(result){
+				client.close();
+				console.log('/main disconnected to MongoDB\n');
+				res.status(200);
+				res.send("OK")
+			});
+		});
+	}
+	else
+		res.status(402);
+});
 
- 			console.log('Connected to MongoDB\n');
- 			insertRecord(db,userInfo,function(result){
- 				client.close();
- 				console.log('/main disconnected to MongoDB\n');
- 				res.status(200);
- 				res.send("OK")
- 			});
- 		});
- 	}
- 	else
- 		res.status(402);
- });
+app.get('/', auth, function(req, res) {
+	MongoClient.connect(mongourl, function(err, client) {
+		assert.equal(null, err);
+		const db = client.db(dbName);
 
- app.get('/', function(req, res) {
- 	if (req.session.authenticated) {
- 		MongoClient.connect(mongourl, function(err, client) {
- 			assert.equal(null, err);
- 			const db = client.db(dbName);
+		findRecords(db, {}, function(result) {
+			client.close();
+			res.render('index', {records: result});
+		});
+	});
+});
 
- 			findRecords(db, {}, function(result) {
- 				client.close();
- 				res.render('index', {records: result});
- 			});
- 		});
- 	} else
- 	res.redirect('/login');
- });
+app.post('/report', auth, function(req, res) {
+	var criteria = {};
+	criteria['username'] = req.body.name;
 
- app.get('/login', function(req, res) {
- 	res.render('login');
- });
+	MongoClient.connect(mongourl, function(err, client) {
+		assert.equal(null, err);
+		const db = client.db(dbName);
 
- app.post('/login', function(req, res) {
- 	var user = {};
- 	user['userid'] = req.body.userid;
- 	user['password'] = req.body.password;
+		findRecords(db, criteria, function(result) {
+			client.close();
+			res.render('report', {records: result});
+		});
+	});
+});
 
- 	MongoClient.connect(mongourl, function(err, client) {
- 		assert.equal(null, err);
- 		const db = client.db(dbName);
+app.get('/login', function(req, res) {
+	if (req.session.authenticated)
+		res.redirect('/');
+	else
+		res.render('login');
+});
 
- 		findUser(db, user, function(result) {
- 			client.close();
- 			if (result) {
- 				req.session.authenticated = true;
- 				req.session._id = result._id;
- 				req.session.userid = result.userid;
- 				res.redirect('/');
- 			} else {
- 				res.render('login', {warning: 'Incorrect user ID or password.'});
- 			}
- 		});
- 	});
- });
+app.post('/login', function(req, res) {
+	var user = {};
+	user['userid'] = req.body.userid;
+	user['password'] = req.body.password;
 
- app.post('/register', function(req, res) {
- 	var user = {};
- 	user['userid'] = req.body.userid;
- 	user['password'] = req.body.password;
+	MongoClient.connect(mongourl, function(err, client) {
+		assert.equal(null, err);
+		const db = client.db(dbName);
 
- 	MongoClient.connect(mongourl, function(err, client) {
- 		assert.equal(null, err);
- 		const db = client.db(dbName);
+		findUser(db, user, function(result) {
+			if (result) {
+				req.session.authenticated = true;
+				req.session.userid = result.userid;
+				
+				findRecords(db, {}, function(result) {
+					client.close();
+					res.render('index', {records: result});
+				});
+			} else {
+				res.render('login', {warning: 'Incorrect user ID or password.'});
+			}
+		});
+	});
+});
 
- 		insertUser(db, user, function(result) {
- 			client.close();
- 			if (result)
- 				res.render('login', {success: 'Registration complete. Please login.'});
- 			else
- 				res.render('login', {warning: 'User ID "' + user['userid'] + '" is taken. Try another.'});
- 		});
- 	});
- });
+app.post('/register', function(req, res) {
+	var user = {};
+	user['userid'] = req.body.userid;
+	user['password'] = req.body.password;
 
- app.post('/logout', function(req, res) {
- 	req.session = null;
- 	res.redirect('/');
- });
+	MongoClient.connect(mongourl, function(err, client) {
+		assert.equal(null, err);
+		const db = client.db(dbName);
 
- app.post('/report', function(req, res) {
- 	if (req.session.authenticated) {
- 		var criteria = {};
- 		criteria['username'] = req.body.name;
+		insertUser(db, user, function(result) {
+			client.close();
+			if (result)
+				res.render('login', {success: 'Registration complete. Please login.'});
+			else
+				res.render('login', {warning: 'User ID "' + user['userid'] + '" is taken. Try another.'});
+		});
+	});
+});
 
- 		MongoClient.connect(mongourl, function(err, client) {
- 			assert.equal(null, err);
- 			const db = client.db(dbName);
-
- 			findRecords(db, criteria, function(result) {
- 				client.close();
- 				res.render('report', {records: result});
- 			});
- 		});
- 	} else
- 	res.redirect('/');
- });
+app.post('/logout', function(req, res) {
+	req.session = null;
+	res.redirect('/login');
+});
 
 // Business Functions
 function insertRecord(db,new_user,callback){
@@ -159,7 +162,7 @@ function insertRecord(db,new_user,callback){
 function findRecords(db, criteria, callback) {
 	var records = [];
 	var cursor = db.collection('records').find(criteria);
-	
+
 	cursor.each(function(err, result) {
 		assert.equal(null, err);
 		if (result != null)
